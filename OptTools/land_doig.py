512236a1c7e95a221c52d1c0453dce49a2479a0d
RodroVMS <democlesvanshepars@gmail.com>
2021-06-28 16:03:59 -0400

    Minor change and improve output printing

--- OptTools/land_doig.py
+++ OptTools/land_doig.py
@@ -1,6 +1,6 @@
 from MyOptHelpers.OptTools.const import MAX_VAL, MIN_VAL
 import numpy as np
-from .utils import add_condition_to_table, basic_solution_str, display_table, get_int, is_feasible_simplex, is_feasible_dual_simplex, get_basic_solution, pure_integer_value, pure_integer_vector, update_table
+from .utils import add_condition_to_table, basic_solution_str, display_table, get_int, is_feasible_simplex, is_feasible_dual_simplex, get_basic_solution, pure_integer_value, pure_integer_vector, update_table, var_name
 from .simplex import simplex
 from .dual_simplex import dual_simplex
 
@@ -9,7 +9,7 @@ def land_doig(func, start_table:np.ndarray, start_base:list, start_slacks:int = 
     total_node_num = int(0) # Pylance victim
     
     output = ""
-    original_vars = len(start_table[0])
+    original_vars = len(start_table[0]) - start_slacks - 1
     x_e = [-1]*original_vars
     xs_e = [-1]*original_vars
     table_e = np.array([])
@@ -18,12 +18,12 @@ def land_doig(func, start_table:np.ndarray, start_base:list, start_slacks:int = 
     number_e = -1
     z_e = MAX_VAL
 
-    pending = [(start_table.copy(), tuple(start_base), start_slacks, total_node_num)]
+    pending = [(start_table.copy(), tuple(start_base), start_slacks, total_node_num, str(""))]
     while len(pending) > 0:
-        table, tup_base, slacks, node_num = pending.pop(-1)
+        table, tup_base, slacks, node_num, trail = pending.pop(-1)
         base = list(tup_base)
         print("\n-------------- -------------- --------------")
-        print(f"Solving Node({node_num})")
+        print(f"Solving Node({node_num}): {trail}")
         display_table(table, base, slacks=slacks)
 
 
@@ -46,7 +46,7 @@ def land_doig(func, start_table:np.ndarray, start_base:list, start_slacks:int = 
         print(f"zk: {z_k}")
 
         if z_k >= z_e:
-            print(f"No improvement in solution: zk({z_k}) >= z_e({z_e})")
+            print(f"No improvement in solution: zk({z_k}) >= ze({z_e})")
             continue
 
         if pure_integer_vector(x_k):
@@ -59,27 +59,39 @@ def land_doig(func, start_table:np.ndarray, start_base:list, start_slacks:int = 
             number_e = node_num
             print(f"Pure integer found. Updating ze = {z_e}")
             continue
-
+        
+        index = len(table[0])
+        min_val = MAX_VAL
         for i, y_i in enumerate(table[:, -1]):
-            if not pure_integer_value(y_i):
-                new_cond_le, new_cond_ge = form_conditon(table, base, i)
-                table_le, base_le, slacks_le = add_condition_to_table(table, base, new_cond_le, slacks)
-                table_ge, base_ge, slacks_ge = add_condition_to_table(table, base, new_cond_ge, slacks)
+            if not pure_integer_value(y_i) and y_i < min_val and i < len(table) - 1:
+                index = i
+                min_val = y_i
 
-                print("Applying dual simplex-ge")
-                table_ge, base_ge = update_table(table_ge, base_ge, base_ge[i], i)
-                table_ge, base_ge, result = dual_simplex(table_ge, base_ge, slacks_ge, display=True)
-                if result:   
-                    pending.append((table_ge, tuple(base_ge), slacks_ge, total_node_num + 2))
-                
-                table_le, base_le =  update_table(table_le, base_le, base_le[i], i)
-                print("Applying dual simplex-le")
-                table_le, base_le, result = dual_simplex(table_le, base_le, slacks_le, display=False)
-                if result:
-                    pending.append((table_le, tuple(base_le), slacks_le, total_node_num + 1))
+        new_cond_le, new_cond_ge = form_conditon(table, base, index)
+        table_le, base_le, slacks_le = add_condition_to_table(table, base, new_cond_le, slacks)
+        table_ge, base_ge, slacks_ge = add_condition_to_table(table, base, new_cond_ge, slacks)
 
-                total_node_num += 2
-                break
+        print("Applying dual simplex-ge")
+        # display_table(table_ge, base_ge, slacks=slacks_ge)
+        # print("Updated:")
+        table_ge, base_ge = update_table(table_ge, base_ge, base_ge[index], index)
+        # display_table(table_ge, base_ge, slacks=slacks_ge)
+        table_ge, base_ge, result = dual_simplex(table_ge, base_ge, slacks_ge, display=True)
+        if result:  
+            total_node_num += 1
+            print(f"Added child({total_node_num})")
+            pending.append((table_ge, tuple(base_ge), slacks_ge, total_node_num, trail + f"{var_name(len(table_ge[0]) -1, slacks_ge, index)} >= {np.floor(min_val) + 1}, "))
+        
+        print("Applying dual simplex-le")
+        # display_table(table_le, base_le, slacks=slacks_le)
+        table_le, base_le =  update_table(table_le, base_le, base_le[index], index)
+        # print("Updated:")
+        # display_table(table_le, base_le, slacks=slacks_le)
+        table_le, base_le, result = dual_simplex(table_le, base_le, slacks_le, display=False)
+        if result:
+            total_node_num += 1 
+            print(f"Added child({total_node_num})")
+            pending.append((table_le, tuple(base_le), slacks_le, total_node_num, trail + f"{var_name(len(table_le[0]) -1, slacks_le, index)} <= {np.floor(min_val)}, "))
         
     print("\n-------------- -------------- --------------")
     if z_e == MAX_VAL:
